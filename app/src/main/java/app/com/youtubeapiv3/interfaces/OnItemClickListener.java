973bc80f69743aa0e5d42e89b3a0fd0a683a5ff6
mrducky <kakashipll@gmail.com>
2018-02-16 03:07:34 +0100

    YT

---
+++ app/src/main/java/app/com/youtubeapiv3/interfaces/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package app.com.youtubeapiv3.interfaces;
+
+
+import app.com.youtubeapiv3.models.YoutubeDataModel;
+
+
+public interface OnItemClickListener {
+    void onItemClick(YoutubeDataModel item);
+
+}
